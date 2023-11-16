ed3a3cc289f06cf38d655cf66fa405974cfc58cb
Esteban Casallas <ecasallas07@gmail.com>
2023-11-16 16:46:01 -0500

    relations/ foreign key

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class User extends Authenticatable
 {
@@ -45,4 +47,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    //HasOne indica relacion de 1 a muchos 
+    public function phone():HasOne
+    {
+        return $this->hasOne(Phone::class,'user_id','id');
+    }
 }
